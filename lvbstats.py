771e1fdb66b4b99b1035fdbabdd7a7a5892f55cb
Mario Wenzel <maweki@gmail.com>
2014-06-14 10:18:51 +0200

    extract login

--- lvbstats.py
+++ lvbstats.py
@@ -1,22 +1,20 @@
 import os
 from twitter import *
 
-CONSUMER_KEY = 'i795JDFmQlZzlfJpIrQQxZ2RP'
-CONSUMER_SECRET = 'ptHQEfq4aBnrUlzX5Wwdlss0iuBaZfhzm9t9OwdAtKqio5UeAC'
+def twitter_login():
+    CONSUMER_KEY = 'i795JDFmQlZzlfJpIrQQxZ2RP'
+    CONSUMER_SECRET = 'ptHQEfq4aBnrUlzX5Wwdlss0iuBaZfhzm9t9OwdAtKqio5UeAC'
 
-target = 'lvb_direkt'
+    MY_TWITTER_CREDS = os.path.expanduser('~/.twitter_oauth_lvbstats')
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance("lvbStats", CONSUMER_KEY, CONSUMER_SECRET,
+                    MY_TWITTER_CREDS)
 
-MY_TWITTER_CREDS = os.path.expanduser('~/.twitter_oauth_lvbstats')
-if not os.path.exists(MY_TWITTER_CREDS):
-    oauth_dance("lvbStats", CONSUMER_KEY, CONSUMER_SECRET,
-                MY_TWITTER_CREDS)
+    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
 
-oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
-
-twitter = Twitter(auth=OAuth(
-    oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
-    
-statuses = twitter.statuses.user_timeline(screen_name=target, count=5)
+    twitter = Twitter(auth=OAuth(
+        oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+    return twitter
 
 def lines_from_text(text):
     return NotImplemented
@@ -33,3 +31,6 @@ def entry_to_tuple(entry):
 
 for s in statuses:
     print(s['text'], s['id'], s['created_at'])
+    
+if __name__ == "__main__":
+    api = twitter_login()
