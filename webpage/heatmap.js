946a4c243877b486bdb2554b59f76685c1f8e6dd
Mario Wenzel <mawe@gmail.com>
2014-07-05 01:06:43 +0200

    Changed Scales

--- webpage/heatmap.js
+++ webpage/heatmap.js
@@ -8,8 +8,11 @@ $.extend(lvbdata,{
       legendElementWidth = gridSize*2,
       buckets = 9,
       colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-      days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-      times = ["0", "1", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+      days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+      times = [];
+      for (var h = 0; h <= 23; h++) {
+        times.push(h.toString() + '-' + ((h + 1)%24).toString());
+      }
 
       var colorScale = d3.scale.quantile()
               .domain([0, buckets - 1, d3.max(data, function (d) { return d.acc; })])
