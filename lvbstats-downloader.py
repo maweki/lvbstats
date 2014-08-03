38b47d04bc30cb8c44e478cbc88603a4bac3c8c0
Mario Wenzel <maweki@gmail.com>
2014-08-03 17:57:28 +0200

    Extract login

--- lvbstats-downloader.py
+++ lvbstats-downloader.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import os
 
 VERSION = '0.3.0'
-from twitter import *
 import lvbstats.paths
+from lvbstats.twit import twitter_login
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -13,23 +12,6 @@ target = 'lvb_direkt'
 shelve_filename = lvbstats.paths.get_shelve_filename()
 
 options = None
-
-
-def twitter_login():
-    CONSUMER_KEY = 'i795JDFmQlZzlfJpIrQQxZ2RP'
-    CONSUMER_SECRET = 'ptHQEfq4aBnrUlzX5Wwdlss0iuBaZfhzm9t9OwdAtKqio5UeAC'
-
-    MY_TWITTER_CREDS = os.path.expanduser('~/.twitter_oauth_lvbstats')
-    if not os.path.exists(MY_TWITTER_CREDS):
-        oauth_dance("lvbStats", CONSUMER_KEY, CONSUMER_SECRET,
-                    MY_TWITTER_CREDS)
-
-    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
-
-    twitter = Twitter(auth=OAuth(
-        oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
-    return twitter
-
 
 class LvbText(object):
     @staticmethod
---
+++ lvbstats/twit.py
@@ -0,0 +1,16 @@
+def twitter_login():
+    import os
+    from twitter import oauth_dance, Twitter, OAuth, read_token_file
+    CONSUMER_KEY = 'i795JDFmQlZzlfJpIrQQxZ2RP'
+    CONSUMER_SECRET = 'ptHQEfq4aBnrUlzX5Wwdlss0iuBaZfhzm9t9OwdAtKqio5UeAC'
+
+    MY_TWITTER_CREDS = os.path.expanduser('~/.twitter_oauth_lvbstats')
+    if not os.path.exists(MY_TWITTER_CREDS):
+        oauth_dance("lvbStats", CONSUMER_KEY, CONSUMER_SECRET,
+                    MY_TWITTER_CREDS)
+
+    oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
+
+    twitter = Twitter(auth=OAuth(
+        oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+    return twitter
