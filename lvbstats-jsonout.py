185acab5a1b2d1474b202bd8d0a0b4f10ee66e6b
Mario Wenzel <maweki@gmail.com>
2014-08-05 17:36:02 +0200

    Show deleted tweets

--- lvbstats-jsonout.py
+++ lvbstats-jsonout.py
@@ -13,6 +13,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Reads @lvb_direkt\'s Twitter feed and saves statistics on it',
                                      epilog=('Version: ' + VERSION))
 
+    parser.add_argument('--deleted', help='Show only deleted tweets', action="store_true")
     parser.add_argument('--nostatic', help='Don\'t print data from static database', action="store_true")
     parser.add_argument('--nolive', help='Don\'t print data from live/stream database', action='store_true')
     parser.add_argument('--jsonstyle', type=str, default='indent', help='Style of JSON (plain or indent)')
@@ -37,17 +38,25 @@ def main():
     db = shelve.open(shelve_filename)
     live_db = shelve.open(shelve_live_filename)
 
-    if not options.nostatic and not options.nolive:
-        uniondict = {}
-        uniondict.update(db)
-        uniondict.update(live_db)
-        print(return_json(uniondict))
+    if options.deleted:
+        deleted_db = {}
+        deleted_keys = set(live_db.keys()) - set(db.keys())
+        for key in deleted_keys:
+            if int(key) < int(db.get_last_tweetid()):
+                deleted_db[key] = live_db[key]
+        print(return_json(deleted_db))
+    else:
+        if not options.nostatic and not options.nolive:
+            uniondict = {}
+            uniondict.update(db)
+            uniondict.update(live_db)
+            print(return_json(uniondict))
 
-    if not options.nostatic and options.nolive:
-        print(return_json(db))
+        if not options.nostatic and options.nolive:
+            print(return_json(db))
 
-    if options.nostatic and not options.nolive:
-        print(return_json(live_db))
+        if options.nostatic and not options.nolive:
+            print(return_json(live_db))
 
     db.close()
     live_db.close()
