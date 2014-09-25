d0e295cca81589caaa3e60dcc4d94f80e810c99e
Mario Wenzel <maweki@gmail.com>
2014-09-25 17:31:55 +0200

    Fixed issue where not tweets were saved

--- lvbstats/tweets.py
+++ lvbstats/tweets.py
@@ -20,15 +20,12 @@ class LvbText(object):
 
     @staticmethod
     def longest_words(text):
-        if not ': ' in text:
-            return None
-        else:
-            from more_itertools import unique_justseen
-            _, _, info_text = text.partition(':')
-            words = sorted((item.strip('".,:!?/ \n()') for item in info_text.split(' ') if not (item.startswith('http://'))),
-                           key=len, reverse=True)
-            unique_words = unique_justseen(words)
-            return list(word for word in unique_words if len(word) > 3)
+        from more_itertools import unique_justseen
+        _, _, info_text = text.partition(':')
+        words = sorted((item.strip('".,:!?/ \n()') for item in info_text.split(' ') if not (item.startswith('http://'))),
+                       key=len, reverse=True)
+        unique_words = unique_justseen(words)
+        return list(word for word in unique_words if len(word) > 3)
 
 def date_from_created_at(cr):
     from datetime import datetime
