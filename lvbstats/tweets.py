8a3b28e94f07407bc66c7b015bdc36d2e6f25d3d
Mario Wenzel <maweki@gmail.com>
2015-09-03 17:33:30 +0200

    Streamwatch and Downloader

--- lvbstats/tweets.py
+++ lvbstats/tweets.py
@@ -1,5 +1,37 @@
+import gzip
+import json
+import os
 import logging
 log = logging.getLogger('lvbstats')
+
+def purge_tweet(orig_tweet):
+    new_tweet = {}
+    for attr in ("truncated", "text", "in_reply_to_status_id",
+                "in_reply_to_screen_name", "created_at", "retweet_count",
+                 "id", "favorite_count", "in_reply_to_user_id"):
+        new_tweet[attr] = orig_tweet[attr]
+
+    new_tweet["user"] = {}
+    for attr in ("followers_count", "friends_count", "id", "statuses_count"):
+        new_tweet["user"][attr] = orig_tweet["user"][attr]
+
+    for attr in ("online"):
+        if attr in orig_tweet:
+            new_tweet[attr] = orig_tweet[attr]
+    return new_tweet
+
+def tweetsaver(overwrite=False):
+    from .paths import get_db_path
+    while True:
+        thistweet = yield
+        purged = purge_tweet(thistweet)
+        tweetid = purged["id"]
+        path = os.path.join(get_db_path(), str(tweetid))
+        if not overwrite and os.path.exists(path):
+            continue
+        with gzip.open(path, 'w') as f:
+            s = json.dumps(purged)
+            f.write(s.encode("utf-8"))
 
 class LvbText(object):
     @staticmethod
