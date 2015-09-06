de2332fd6e58047b9e382564360c9b062a310127
Mario Wenzel <maweki@gmail.com>
2015-09-06 17:31:30 +0200

    Removed unused code

--- lvbstats/tweets.py
+++ lvbstats/tweets.py
@@ -2,8 +2,6 @@ import gzip
 import json
 import os
 from functools import wraps
-import logging
-log = logging.getLogger('lvbstats')
 
 def prime(fun):
     @wraps(fun)
@@ -44,44 +42,6 @@ def tweetsaver(overwrite=False):
         with gzip.open(path, 'w') as f:
             s = json.dumps(purged)
             f.write(s.encode("utf-8"))
-
-class LvbText(object):
-    @staticmethod
-    def lines_from_text(text):
-        if not ': ' in text:
-            return []
-        else:
-            def to_int_optional(num):
-                if str(num).isdigit():
-                    return int(num)
-                else:
-                    return str(num)
-
-            lines_str, _, _ = text.partition(':')
-            lines = list(to_int_optional(l) for l in lines_str.split(', ') if len(l) <= 3)
-            return lines
-
-    @staticmethod
-    def longest_words(text):
-        from more_itertools import unique_justseen
-        info_text = text
-        words = sorted((item.strip('".,:!?/ \n()') for item in info_text.split(' ') if not (item.startswith('http://'))),
-                       key=len, reverse=True)
-        unique_words = unique_justseen(words)
-        return list(word for word in unique_words if len(word) > 3)
-
-def date_from_created_at(cr):
-    from datetime import datetime
-
-    created_at = datetime.strptime(cr, '%a %b %d %X %z %Y').timestamp()
-    return created_at
-
-def split_text(text):
-    if not ': ' in text:
-        return '', text
-    lines, _, info_text = text.partition(':')
-    info_text = info_text.strip()
-    return lines, info_text
 
 def get_match(haystack, needle):
     if len(haystack.strip()) < len(needle):
@@ -141,33 +101,3 @@ def query_web(text):
         except UnicodeDecodeError as e:
             log.error((UnicodeDecodeError, e, 'page headers', page.getheaders()))
             raise
-
-def entry_to_tuple(entry, _query_web=False):
-    entry_id = entry['id']
-    lines = LvbText.lines_from_text(entry['text'])
-    if lines and _query_web and '...' in entry['text']:
-        _, text = split_text(entry['text'])
-        retries = 4
-        while retries:
-            from time import sleep
-            sleep(10)
-            retries -= 1
-            try:
-                webresult = query_web(text[0:-26])
-                if webresult:
-                    text = webresult
-                    break
-                continue
-            except Exception as e:
-                log.error((e, type(e)))
-                continue
-        else:
-            log.info('Couldn\'t retrieve page')
-    else:
-        _, text = split_text(entry['text'])
-        text = text[:-22]
-    text = text.strip()
-    log.debug(text)
-
-    return entry_id, (date_from_created_at(entry['created_at']),
-        lines, LvbText.longest_words(text), text)
